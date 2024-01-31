610040d8d31c2bde6fb6c7fd544c0a68479394f8
root <root@thoomer>
2024-01-30 23:25:48 -0600

    transferring to css file

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,35 +7,35 @@ import imageTwo from '../images/house1nice.jpg'
 
 function App() {
   return (
-    <div className="App bg-gray-200 min-h-screen py-12">
+    <div className="App">
 
-      <nav className="bg-blue-700 p-6 text-white shadow-md">
-        <div className="container mx-auto">
-          <h1 className="text-2xl font-bold mb-4">Safer Powerwashing</h1>
-          <div className="flex space-x-6">
-            <a href="#about" className="hover:bg-blue-800 px-4 py-2 rounded transition duration-300 ease-in-out">About Us</a>
-            <a href="#services" className="hover:bg-blue-800 px-4 py-2 rounded transition duration-300 ease-in-out">Services</a>
-            <a href="#testimonials" className="hover:bg-blue-800 px-4 py-2 rounded transition duration-300 ease-in-out">Testimonials</a>
-            <a href="#contact" className="hover:bg-blue-800 px-4 py-2 rounded transition duration-300 ease-in-out">Contact</a>
+      <nav className="navbar">
+        <div className="auto">
+          <h1 className="h1">Safer Powerwashing</h1>
+          <div className="inner">
+            <a href="#about" className="a1">About Us</a>
+            <a href="#services" className="a2">Services</a>
+            <a href="#testimonials" className="a3">Testimonials</a>
+            <a href="#contact" className="a4">Contact</a>
           </div>
         </div>
       </nav>
 
-      <section id="about" className="p-8 bg-white rounded-lg shadow-md my-8 mx-4">
-        <h2 className="text-3xl font-bold mb-4">About Us</h2>
+      <section id="about" className="About">
+        <h2 className="us2">About Us</h2>
         <p>
         "Safer Powerwashing isn't just our name, it's our promise. With a fusion of seasoned expertise and eco-friendly techniques, 
         we rejuvenate surfaces while safeguarding the environment. Our commitment goes beyond the shine; it's about providing a cleaner, 
         Safer, and brighter community for everyone. Dive deeper into our story and discover why Safer Powerwashing has become the trusted 
         choice for transformative cleaning in Baton Rouge."
         </p>
-        <img src={'/snickles.jpg'} alt="Test" />
+        <img src={'/snickles.jpg'} alt="numba1" />
 
       </section>
 
-      <section id="services" className="p-8 bg-white rounded-lg shadow-md my-8 mx-4">
-        <h2 className="text-3xl font-bold mb-4">Services</h2>
-        <ul className="list-disc pl-8">
+      <section id="services" className="Services">
+        <h2 className="forYou">Services</h2>
+        <ul className="list2">
           <li className="mb-2">Residential Powerwashing</li>
           <li className="mb-2">Commercial Powerwashing</li>
           <li className="mb-2">Driveway and Patio Cleaning</li>
